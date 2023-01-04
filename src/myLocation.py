5fa4380e61cf7f573b9b9a0113c3f9c26d358419
KDubicki <kamil3234@gmail.com>
2023-01-04 14:13:59 +0100

    fix: add some object and call method myLocation

--- src/myLocation.py
+++ src/myLocation.py
@@ -8,3 +8,11 @@ class Location:
         print(f'Hi, my name is {self.name} and  I live in {self.country}.')
 
 
+loc1 = Location("Tomas", "Portugal")
+loc1.myLocation()
+loc2 = Location("Ying", "China")
+loc3 = Location("Amare", "Kenya")
+loc2.myLocation()
+loc3.myLocation()
+your_loc = Location("Your_Name", "Your_Country")
+your_loc.myLocation()
